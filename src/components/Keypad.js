968af1b62758cf5dfcc6e620356e06f127bd18b4
louis baptiste <louisvis@ymail.com>
2020-07-05 19:04:02 +0000

    Automatically backed up by Learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,13 @@
 // Code Keypad Component Here
+class Keypad extends React.Component {
+
+  tickle = () => {
+    console.log('Tee hee!');
+  }
+
+  render() {
+    return (
+      <input type="password"/>
+    );
+  }
+}
